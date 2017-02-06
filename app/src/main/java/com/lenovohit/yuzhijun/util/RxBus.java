f5202c750839efaa23b56e53d718ea216a3ef64f
yuzhijun <664281688@qq.com>
2017-02-06 09:45:56 +0800

    增加rxbus的sticky事件

--- app/src/main/java/com/lenovohit/yuzhijun/util/RxBus.java
+++ app/src/main/java/com/lenovohit/yuzhijun/util/RxBus.java
@@ -1,6 +1,10 @@
 package com.lenovohit.yuzhijun.util;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import rx.Observable;
+import rx.Subscriber;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
 import rx.subjects.Subject;
@@ -9,33 +13,111 @@ import rx.subjects.Subject;
  * Created by lenovo on 2016/3/30.
  */
 public class RxBus {
-    private static volatile RxBus defaultInstance;
-    // 主题
-    private final Subject bus;
-    // PublishSubject只会把在订阅发生的时间点之后来自原始Observable的数据发射给观察者
+    private static volatile RxBus mDefaultInstance;
+    private final Subject<Object, Object> mBus;
+
+    private final Map<Class<?>, Object> mStickyEventMap;
+
     public RxBus() {
-        bus = new SerializedSubject<>(PublishSubject.create());
+        mBus = new SerializedSubject<>(PublishSubject.create());
+        mStickyEventMap = new ConcurrentHashMap<>();
     }
-    // 单例RxBus
+
     public static RxBus getDefault() {
-        RxBus rxBus = defaultInstance;
-        if (defaultInstance == null) {
+        if (mDefaultInstance == null) {
             synchronized (RxBus.class) {
-                rxBus = defaultInstance;
-                if (defaultInstance == null) {
-                    rxBus = new RxBus();
-                    defaultInstance = rxBus;
+                if (mDefaultInstance == null) {
+                    mDefaultInstance = new RxBus();
                 }
             }
         }
-        return rxBus;
+        return mDefaultInstance;
     }
-    // 提供了一个新的事件
-    public void post (Object o) {
-        bus.onNext(o);
+
+    /**
+     * 发送事件
+     */
+    public void post(Object event) {
+        mBus.onNext(event);
     }
-    // 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者
-    public <T> Observable<T> toObserverable (Class<T> eventType) {
-        return bus.ofType(eventType);
+
+    /**
+     * 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者
+     */
+    public <T> Observable<T> toObservable(Class<T> eventType) {
+        return mBus.ofType(eventType);
+    }
+
+    /**
+     * 判断是否有订阅者
+     */
+    public boolean hasObservers() {
+        return mBus.hasObservers();
+    }
+
+    public void reset() {
+        mDefaultInstance = null;
+    }
+
+    /**
+     * Stciky 相关
+     */
+
+    /**
+     * 发送一个新Sticky事件
+     */
+    public void postSticky(Object event) {
+        synchronized (mStickyEventMap) {
+            mStickyEventMap.put(event.getClass(), event);
+        }
+        post(event);
+    }
+
+    /**
+     * 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者
+     */
+    public <T> Observable<T> toObservableSticky(final Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            Observable<T> observable = mBus.ofType(eventType);
+            final Object event = mStickyEventMap.get(eventType);
+
+            if (event != null) {
+                return observable.mergeWith(Observable.create(new Observable.OnSubscribe<T>() {
+                    @Override
+                    public void call(Subscriber<? super T> subscriber) {
+                        subscriber.onNext(eventType.cast(event));
+                    }
+                }));
+            } else {
+                return observable;
+            }
+        }
+    }
+
+    /**
+     * 根据eventType获取Sticky事件
+     */
+    public <T> T getStickyEvent(Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            return eventType.cast(mStickyEventMap.get(eventType));
+        }
+    }
+
+    /**
+     * 移除指定eventType的Sticky事件
+     */
+    public <T> T removeStickyEvent(Class<T> eventType) {
+        synchronized (mStickyEventMap) {
+            return eventType.cast(mStickyEventMap.remove(eventType));
+        }
+    }
+
+    /**
+     * 移除所有的Sticky事件
+     */
+    public void removeAllStickyEvents() {
+        synchronized (mStickyEventMap) {
+            mStickyEventMap.clear();
+        }
     }
 }
